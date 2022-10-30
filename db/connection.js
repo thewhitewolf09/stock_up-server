a36605135097e49ee3a70c94bdd1a7b0b272e2db
thewhitewolf09 <sumitramprakashnirmal@gmail.com>
2022-10-30 12:02:24 +0530

    first commit

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const DB = process.env.DATABASE
 
-mongoose.connect(DB).then(() => {
+mongoose.connect("mongodb://127.0.0.1:27017/test").then(() => {
     console.log('Connection successful');
 }).catch((err) => {
     console.log(err);
